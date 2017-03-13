06f348ed2c9778dc5436f15ea11e32a90c0aab9f
Ciara Burkett <ciaraburkett@gmail.com>
2017-03-13 10:08:31 -0400

    Initial commit

--- Temperature Converter/ViewController.swift
+++ Temperature Converter/ViewController.swift
@@ -9,10 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var count: Int = 0
+    
+    @IBOutlet weak var conversionLabel: UILabel!
+    
+    @IBOutlet weak var tempEntry: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func convertClicked(_ sender: Any) {
+        print("Convert clicked.")
+        
+        if let result = tempEntry.text {
+            if (result == "") {
+                return
+            }
+            else {
+                if let num = Double(result) {
+                    let output = num * (9/5) + 32
+                    resultLabel.text = String(output)
+                    
+                    countPlus1()
+                }
+            }
+        }
+    }
+    
+    func countPlus1() {
+        count += 1
+        conversionLabel.text = String(count) + " Conversions"
     }
 
     override func didReceiveMemoryWarning() {
